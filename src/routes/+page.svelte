9647b4b0b6d8fd229436d7ff271cccfc394aa098
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 19:50:56 +1100

    Add external pico stylesheet for svelte app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css">
+</svelte:head>
