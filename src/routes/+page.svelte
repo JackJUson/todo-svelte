513f1b39cdab0be34df75055d6b63d82974614c8
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 20:18:24 +1100

    Add todo list variable and warp around list item div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,29 @@
+<script>
+	let toDoList = [1,2,{ content: 'Grind or go home!', editing: false, checked: false }]; // Arry of ToDos
+</script>
+
 <svelte:head>
-  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css">
+	<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css" />
 </svelte:head>
 
 <!-- ToDo List Input  -->
 <div style="margin: 0 auto; padding: 20px; width: 700px;">
-  <h2 style="text-align: center;">ToDo List</h2>
-  <p>Enter your ToDo here</p>
-  <div style="display: flex;">
-    <input type="text">
-    <button style="width: 200px; margin-left: 24px;">Add</button>
-  </div>
+	<h2 style="text-align: center;">ToDo List</h2>
+	<p>Enter your ToDo here</p>
+	<div style="display: flex;">
+		<input type="text" />
+		<button style="width: 200px; margin-left: 24px;">Add</button>
+	</div>
 </div>
 
 <!-- ToDo List Item -->
-<div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;">
-  <input type="checkbox">
-  <h4 style="flex-grow: 1">Grind everyday!!!!!</h4>
-  <div style="display: flex;">
-    <button>Edit</button>
-    <button style="margin-left: 24px;">Delete</button>
-  </div>
-</div>
+{#each toDoList as toDo, i}
+	<div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;">
+		<input type="checkbox" />
+		<h4 style="flex-grow: 1">Grind everyday!!!!!</h4>
+		<div style="display: flex;">
+			<button>Edit</button>
+			<button style="margin-left: 24px;">Delete</button>
+		</div>
+	</div>
+{/each}
