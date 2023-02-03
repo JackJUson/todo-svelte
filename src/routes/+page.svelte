f7a619bfdaf52c5b8891ffcd4a34bc41981191c2
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 20:03:35 +1100

    Add text input and button to todo list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,8 @@
 <div style="margin: 0 auto; padding: 20px; width: 700px;">
   <h2 style="text-align: center;">ToDo List</h2>
   <p>Enter your ToDo here</p>
+  <div style="display: flex;">
+    <input type="text">
+    <button style="width: 200px; margin-left:24px;">Add</button>
+  </div>
 </div>
