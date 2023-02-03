200369c61c6d5620c90c9a48ba8e109a41a38685
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 19:59:43 +1100

    Add header and description to todo list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <svelte:head>
   <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css">
 </svelte:head>
+
+<div style="margin: 0 auto; padding: 20px; width: 700px;">
+  <h2 style="text-align: center;">ToDo List</h2>
+  <p>Enter your ToDo here</p>
+</div>
